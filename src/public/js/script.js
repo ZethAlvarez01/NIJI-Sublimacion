b346f63dc130af5aac6fa36b3888f0ff41591173
ZethAlvarez01 <zethalvarezh@hotmail.com>
2020-06-19 02:53:38 -0500

    Estilos menu aun no responsive

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -3,3 +3,14 @@ function prueba(){
     console.log("Hola");
     
 }
+
+function notification(){
+    let noti = document.getElementById('noti');
+
+    if(noti.innerHTML == "0"){
+        noti.style.visibility = "hidden";
+    }else{
+        noti.style.visibility = "visible";
+    }
+
+}
