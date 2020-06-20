0a1c1a913b6e301bb2b935d18457a9960802c357
ZethAlvarez01 <zethalvarezh@hotmail.com>
2020-06-20 03:20:28 -0500

    menu desplegable css, falta icono

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,8 +1,19 @@
-function prueba(){
 
-    console.log("Hola");
+function desplegar(){
+
+    let menu = document.getElementById("menuh");
+    let menuf = document.getElementById("fondoF");
     
+    if(menu.classList.contains("active")){
+        menu.classList.remove("active");
+        menuf.classList.remove("activef");
+    }else{
+        menu.classList.add("active");
+        menuf.classList.add("activef");
+    }
+
 }
+
 
 function notification(){
     let noti = document.getElementById('noti');
