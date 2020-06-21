52edb497dc7623fc5969164c58c9cc07b6351fa2
ZethAlvarez01 <zethalvarezh@hotmail.com>
2020-06-20 23:58:20 -0500

    footer y menu listo

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -3,13 +3,43 @@ function desplegar(){
 
     let menu = document.getElementById("menuh");
     let menuf = document.getElementById("fondoF");
+    let face = document.getElementById("face-li");
+    let what = document.getElementById("what");
+    let goo = document.getElementById("google");
     
     if(menu.classList.contains("active")){
+        face.classList.remove("aparecer");
+        face.classList.add("lista-li-color");
+        face.classList.remove("indice");
+
+        what.classList.remove("aparecer");
+        what.classList.add("lista-li-color");
+        what.classList.remove("indice");
+
+        goo.classList.remove("aparecer");
+        goo.classList.add("lista-li-color");
+        goo.classList.remove("indice");
+
         menu.classList.remove("active");
         menuf.classList.remove("activef");
+        
+        
     }else{
+        face.classList.add("aparecer");
+        face.classList.remove("lista-li-color");
+        face.classList.add("indice");
+
+        what.classList.add("aparecer");
+        what.classList.remove("lista-li-color");
+        what.classList.add("indice");
+
+        goo.classList.add("aparecer");
+        goo.classList.remove("lista-li-color");
+        goo.classList.add("indice");
+
         menu.classList.add("active");
         menuf.classList.add("activef");
+        
     }
 
 }
